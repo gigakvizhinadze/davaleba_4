93d0d2f2f85022665bd764cffc6f1b0f876c0f3a
giga kvizhinadze <158417259+gigakvizhinadze@users.noreply.github.com>
2024-02-26 09:03:24 +0400

    Add files via upload

---
+++ SQL_davaleba_1.sql
@@ -0,0 +1,16 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    Name VARCHAR(100),
+    Price DECIMAL(10, 2),
+    Quantity INT,
+    Color VARCHAR(50)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
